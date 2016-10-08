afa8bdaa98a97cdf630908def11cc095c5891e5d
Artyom Vakulin <vakulin95@outlook.com>
2016-10-09 01:38:54 +0300

    Added discrete model

--- anim_pl.gnu
+++ anim_pl.gnu
@@ -1,12 +1,12 @@
 set term gif \
     animate \
     optimize \
-    delay 10 \
-    size 900, 700 \
+    delay 1 \
+    size 1600, 900 \
     background "white" \
     crop \
-    font "Times-Roman,10"
-set xrange [0 : 3]
+    font "Times-Roman,14"
+set xrange [0 : 1]
 set yrange[-5 : 5]
 set xzeroaxis
 set style data lines
@@ -15,22 +15,20 @@ set xtics auto
 set ytics auto
 set mxtics
 set mytics
-set title "Решение непрерывной модели"
+
+set title "Малые поперечные колебания струны с закрепленными концами"
 set xlabel "x"
 set ylabel "u"
 
 plot_cont = "files/cont_out.txt"
-title_cont = "Амплитуда u(x, t)"
+plot_discr = "files/discr_out.txt"
+title_cont = "Решение непрерывной модели"
+title_discr = "Решение дискретной модели"
 
-set output "animate_cont.gif"
+set output "animate_out.gif"
 
-#do for [i = 0:299]{
-#    plot_cont = sprintf("files/data/cont_out%d.txt", i)
-#    plot plot_cont u 1:2 title title lc rgb "red"
-#}
-
-do for [i = 0:299]{
-    plot plot_cont u 1:i title title_cont lc rgb "red"
+do for [i = 0:999]{
+    plot plot_cont u 1:i title title_cont lc rgb "red" , plot_discr u 1:i title title_discr lc rgb "blue"
 }
 
 unset out
--- main.c
+++ main.c
@@ -3,38 +3,63 @@
 
 #define PI 3.14159265
 
-#define PROP_A 20.0
+#define A_PROP 10.0
 #define ATT_LOC1 0
-#define ATT_LOC2 3.0
+#define ATT_LOC2 1.0
 #define XLEN ( ATT_LOC2 - ATT_LOC1 )
 #define TIME_LOC1 0
-#define TIME_LOC2 3.0
+#define TIME_LOC2 1.0
 #define TLEN ( TIME_LOC2 - TIME_LOC1 )
 
 #define TERMS_NUM 10
 
-#define XDOTS_NUM 300
+#define XDOTS_NUM 100
 #define DX ( XLEN / (XDOTS_NUM - 1) )
-#define TDOTS_NUM 300
+#define TDOTS_NUM 1000
 #define DT ( TLEN / (TDOTS_NUM - 1) )
 
-double phi(double, double);
-double ksi(double, double);
+double phi(double);
+double phi_s(double, double);
+double ksi(double);
+double ksi_s(double, double);
 double integration(double, double, double, double(*f)(double, double));
 void continuous_model(double u[XDOTS_NUM][TDOTS_NUM]);
-int write(double u[XDOTS_NUM][TDOTS_NUM], char*);
+int discrete_model(double u[XDOTS_NUM][TDOTS_NUM]);
+int write(double u[XDOTS_NUM][TDOTS_NUM], char*, int);
 
 int main()
 {
     double u[XDOTS_NUM][TDOTS_NUM];
 
+    if((discrete_model(u) != 0) || (write(u, "discr", 1000)))
+        return 0;
+
     continuous_model(u);
-    write(u, "cont");
+    write(u, "cont", 1000);
 
     return 0;
 }
 
-double phi(double x, double k)
+double phi(double x)
+{
+    double Y;
+
+    // if(x >= 0 && x <= XLEN / 2)
+    //     Y = 0.2 * x;
+    // else if(x >= XLEN / 2 && x <= XLEN)
+    //     Y = -0.2 * (x - XLEN);
+    // else
+    // {
+    //     printf("Error in phi!\n");
+    //     return 0;
+    // }
+
+    Y = 2 * sin(PI * x / XLEN);
+
+    return Y;
+}
+
+double phi_s(double x, double k)
 {
     double Y;
 
@@ -53,7 +78,19 @@ double phi(double x, double k)
     return Y * sin(k * PI * x / XLEN);
 }
 
-double ksi(double x, double k)
+double ksi(double x)
+{
+    double Y = 3;
+
+    if(x >= XLEN/3 && x <= 2.0*XLEN/3)
+        Y = 10.0;
+    else
+        Y = 0;
+
+    return Y;
+}
+
+double ksi_s(double x, double k)
 {
     double Y = 3;
 
@@ -83,7 +120,7 @@ double integration(double a, double b, double k, double(*f)(double, double))
     }
 
     Y = h / 3 * ( f(a, k) + f(b, k) + S);
-    //printf("%f\n", Y);
+
     return Y;
 }
 
@@ -102,20 +139,57 @@ void continuous_model(double u[XDOTS_NUM][TDOTS_NUM])
             x = i * DX;
             for(k = 1; k < TERMS_NUM; k++)
             {
-                a = (2.0 / XLEN) * integration(ATT_LOC1, ATT_LOC2, k, phi);
-                b = (2.0 / (k * PI * PROP_A)) * integration(ATT_LOC1, ATT_LOC2, k, ksi);
+                a = (2.0 / XLEN) * integration(ATT_LOC1, ATT_LOC2, k, phi_s);
+                b = (2.0 / (k * PI * A_PROP)) * integration(ATT_LOC1, ATT_LOC2, k, ksi_s);
 
-                u[i][j] += (a * cos(k * PI * PROP_A * t / XLEN) + b * sin(k * PI * PROP_A * t / XLEN)) * sin(k * PI * x / XLEN);
+                u[i][j] += (a * cos(k * PI * A_PROP * t / XLEN) + b * sin(k * PI * A_PROP * t / XLEN)) * sin(k * PI * x / XLEN);
             }
         }
     }
 }
 
-int write(double u[XDOTS_NUM][TDOTS_NUM], char *str)
+int discrete_model(double u[XDOTS_NUM][TDOTS_NUM])
+{
+    int i, j;
+    double r, x;
+
+    if((r = A_PROP * DT / DX) > 1)
+    {
+        printf("Incorrect values for discrete model!\nr = %.3f\nTry XDOTS_NUM <= %d; or A_PROP <= %d\n",
+        r, (int)(XLEN / (A_PROP * DT) + 1), (int)(XLEN / ((XDOTS_NUM - 1) * DT)));
+        return 1;
+    }
+
+    for(i = 0; i < XDOTS_NUM; i++)
+    {
+        x = i * DX;
+        u[i][0] = phi(x);
+        u[i][1] = u[i][0] + DT * ksi(x);
+    }
+
+    for(j = 2; j < TDOTS_NUM; j++)
+    {
+        u[0][j] = u[XDOTS_NUM - 1][j] = 0;
+        for(i = 1; i < XDOTS_NUM - 1; i++)
+        {
+            u[i][j] = 2 * (1 - pow(r, 2)) * u[i][j - 1] + pow(r, 2) * (u[i + 1][j - 1] + u[i - 1][j - 1]) - u[i][j - 2];
+        }
+    }
+
+    return 0;
+}
+
+int write(double u[XDOTS_NUM][TDOTS_NUM], char *str, int time_top)
 {
     int i, j;
     char filename[50];
     FILE *out;
+
+    if(time_top > TDOTS_NUM)
+    {
+        printf("Error in func write!\nTry time_top <= %d\n", TDOTS_NUM);
+        return 2;
+    }
 
     sprintf(filename, "files/%s_out.txt", str);
 
@@ -127,9 +201,9 @@ int write(double u[XDOTS_NUM][TDOTS_NUM], char *str)
 
     for(i = 0; i < XDOTS_NUM; i++)
     {
-        fprintf(out, "%6.2f", DX * i);
-        for(j = 0; j < TDOTS_NUM; j++)
-            fprintf(out, "%6.2f", u[i][j]);
+        fprintf(out, "%7.3f", DX * i);
+        for(j = 0; j < time_top; j++)
+            fprintf(out, "%7.3f", u[i][j]);
         fprintf(out, "\n");
     }
 
