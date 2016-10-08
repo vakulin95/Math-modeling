3e5b6a6fbb5befcd13bd7a556cba10e7af3e9e38
Artyom Vakulin <vakulin95@outlook.com>
2016-10-08 20:33:59 +0300

    continious full working

--- main.c
+++ main.c
@@ -21,28 +21,15 @@
 double phi(double, double);
 double ksi(double, double);
 double integration(double, double, double, double(*f)(double, double));
-//void continuous_model(double*, double);
 void continuous_model(double u[XDOTS_NUM][TDOTS_NUM]);
-//int write(double*, char*, double);
 int write(double u[XDOTS_NUM][TDOTS_NUM], char*);
 
 int main()
 {
-    int i = 0;
     double u[XDOTS_NUM][TDOTS_NUM];
 
     continuous_model(u);
     write(u, "cont");
-
-    //double t = 0;
-
-    // for(t = 0; t <= 3; t += 0.01)
-    // {
-    //     continuous_model(u, t);
-    //     if(write(u, "cont", i) == 1)
-    //         return 1;
-    //     i++;
-    // }
 
     return 0;
 }
@@ -100,26 +87,6 @@ double integration(double a, double b, double k, double(*f)(double, double))
     return Y;
 }
 
-// void continuous_model(double *u, double t)
-// {
-//     int i;
-//     double x, a, b, k;
-//
-//     for(i = 0; i < XDOTS_NUM; i++)
-//     {
-//         u[i] = 0;
-//         x = i * DX;
-//         for(k = 1; k < TERMS_NUM; k++)
-//         {
-//             a = (2.0 / XLEN) * integration(ATT_LOC1, ATT_LOC2, k, phi);
-//             b = (2.0 / (k * PI * PROP_A)) * integration(ATT_LOC1, ATT_LOC2, k, ksi);
-//             // printf("%f %f\n", a, b);
-//
-//             u[i] += (a * cos(k * PI * PROP_A * t / XLEN) + b * sin(k * PI * PROP_A * t / XLEN)) * sin(k * PI * x / XLEN);
-//         }
-//     }
-// }
-
 void continuous_model(double u[XDOTS_NUM][TDOTS_NUM])
 {
     int i, j;
@@ -137,38 +104,12 @@ void continuous_model(double u[XDOTS_NUM][TDOTS_NUM])
             {
                 a = (2.0 / XLEN) * integration(ATT_LOC1, ATT_LOC2, k, phi);
                 b = (2.0 / (k * PI * PROP_A)) * integration(ATT_LOC1, ATT_LOC2, k, ksi);
-                // printf("%f %f\n", a, b);
 
                 u[i][j] += (a * cos(k * PI * PROP_A * t / XLEN) + b * sin(k * PI * PROP_A * t / XLEN)) * sin(k * PI * x / XLEN);
             }
         }
     }
 }
-
-// int write(double *u, char *str, double num)
-// {
-//     int i;
-//     double x;
-//     char filename[50];
-//     FILE *out;
-//
-//     sprintf(filename, "files/data/%s_out%.0f.txt", str, num);
-//
-//     if( (out = fopen(filename, "w")) == NULL )
-//     {
-//         printf("Opening file error!");
-//         return 1;
-//     }
-//
-//     for(i = 0; i < XDOTS_NUM; i++)
-//     {
-//         x = DX * i;
-//         fprintf(out, "%.3f %.3f\n", x, u[i]);
-//     }
-//
-//     fclose(out);
-//     return 0;
-// }
 
 int write(double u[XDOTS_NUM][TDOTS_NUM], char *str)
 {
