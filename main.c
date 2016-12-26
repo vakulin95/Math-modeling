15b3a1fdd0fa2b66cce755ae28f3fc26b68f1776
Artyom Vakulin <vakulin95@outlook.com>
2016-12-26 21:24:03 +0300

    ode solving working a bit

--- main.c
+++ main.c
@@ -4,51 +4,99 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv2.h>
 
-int func (double t, const double y[], double f[], void *params)
+#define TETA_PROP   (10.0)
+#define R_PROP      (28.0)
+#define B_PROP      (8.0 / 3.0)
+
+#define STEP        (1e-6)
+
+#define INIT_X      (10.0)
+#define INIT_Y      (0.0)
+#define INIT_Z      (0.0)
+
+int func(double t, const double y[], double f[], void *params)
 {
     (void)(t); /* avoid unused parameter warning */
-    double mu = *(double *)params;
 
-    f[0] = y[1];
-    f[1] = -y[0] - mu*y[1]*(y[0]*y[0] - 1);
+    double teta = ((double *)params)[0];
+    double r = ((double *)params)[1];
+    double b = ((double *)params)[2];
+
+    /*  x = y[0]
+        y = y[1]
+        z = y[2]
+    */
+
+    f[0] = -teta * y[0] + teta * y[1];
+    f[1] = -y[0] * y[2] + r * y[0] - y[1];
+    f[2] = y[0] * y[1] - b * y[2];
 
     return GSL_SUCCESS;
 }
 
-int jac (double t, const double y[], double *dfdy, double dfdt[], void *params)
+int jac(double t, const double y[], double *dfdy, double dfdt[], void *params)
 {
-  (void)(t); /* avoid unused parameter warning */
-  double mu = *(double *)params;
+    (void)(t); /* avoid unused parameter warning */
 
-  gsl_matrix_view dfdy_mat = gsl_matrix_view_array (dfdy, 2, 2);
-  gsl_matrix * m = &dfdy_mat.matrix;
+    double teta = ((double *)params)[0];
+    double r = ((double *)params)[1];
+    double b = ((double *)params)[2];
 
-  gsl_matrix_set (m, 0, 0, 0.0);
-  gsl_matrix_set (m, 0, 1, 1.0);
-  gsl_matrix_set (m, 1, 0, -2.0*mu*y[0]*y[1] - 1.0);
-  gsl_matrix_set (m, 1, 1, -mu*(y[0]*y[0] - 1.0));
+    gsl_matrix_view dfdy_mat = gsl_matrix_view_array (dfdy, 3, 3);
+    gsl_matrix *jacobi = &dfdy_mat.matrix;
 
-  dfdt[0] = 0.0;
-  dfdt[1] = 0.0;
+    gsl_matrix_set (jacobi, 0, 0, -teta);
+    gsl_matrix_set (jacobi, 0, 1, teta);
+    gsl_matrix_set (jacobi, 0, 2, 0.0);
 
-  return GSL_SUCCESS;
+    gsl_matrix_set (jacobi, 1, 0, -y[2] + r);
+    gsl_matrix_set (jacobi, 1, 1, -1.0);
+    gsl_matrix_set (jacobi, 1, 2, -y[0]);
+
+    gsl_matrix_set (jacobi, 2, 0, y[1]);
+    gsl_matrix_set (jacobi, 2, 1, y[0]);
+    gsl_matrix_set (jacobi, 2, 2, -b);
+
+    dfdt[0] = 0.0;
+    dfdt[1] = 0.0;
+
+    return GSL_SUCCESS;
 }
 
 int main(void)
 {
-  double mu = 10;
-  gsl_odeiv2_system sys = {func, jac, 2, &mu};
+    int i;
+    int status;
 
-  gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk8pd, 1e-6, 1e-6, 0.0);
+    double t = 0.0, t1 = 100.0, ti;
 
-  int i;
-  double t = 0.0, t1 = 100.0;
-  double y[2] = {1.0, 0.0};
+    double y[3]   = {
+                        INIT_X,
+                        INIT_Y,
+                        INIT_Z
+                    };
+
+    double params[3];
+
+    params[0] = TETA_PROP;
+    params[1] = R_PROP;
+    params[2] = B_PROP;
+
+    gsl_odeiv2_system ode_system;
+
+    ode_system.function = func;
+    ode_system.jacobian = jac;
+    ode_system.dimension = 3;
+    ode_system.params = params;
+
+    gsl_odeiv2_driver *driver;
+
+    driver = gsl_odeiv2_driver_alloc_y_new(&ode_system, gsl_odeiv2_step_rk8pd, STEP, 1e-6, 0.0);
 
     for (i = 1; i <= 100; i++)
     {
-        double ti = i * t1 / 100.0;
-        int status = gsl_odeiv2_driver_apply (d, &t, ti, y);
+        ti = i * t1 / 100.0;
+        status = gsl_odeiv2_driver_apply (driver, &t, ti, y);
 
         if (status != GSL_SUCCESS)
         {
@@ -56,9 +104,10 @@ int main(void)
             break;
         }
 
-        printf ("%.5e %.5e %.5e\n", t, y[0], y[1]);
+        printf ("%.5lf %.5lf %.5lf %.5lf\n", t, y[0], y[1], y[2]);
     }
 
-  gsl_odeiv2_driver_free (d);
-  return 0;
+    gsl_odeiv2_driver_free (driver);
+
+    return 0;
 }
